685153b87d12a2bb0cbb6a3f340c5ba5936976f2
Sandro Andre Janotte <sjanotte@gmail.com>
2022-12-18 20:34:39 -0300

    ajustes de gateway

--- gateway/src/main/java/br/edu/infnet/gateway/GatewayApplication.java
+++ gateway/src/main/java/br/edu/infnet/gateway/GatewayApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 @SpringBootApplication
@@ -16,20 +18,31 @@ public class GatewayApplication {
 
 	@Bean
 	public RouteLocator routes(RouteLocatorBuilder builder) {
-
 		return builder.routes()
-				.route(p -> p.path("/ordens/**").uri("http://localhost:8190"))
-				.route(p -> p.path("/usuarios/**").uri("http://localhost:8191"))
-				.route(p -> p.path("/pessoas/**").uri("http://localhost:8192"))
-				.route(p -> p.path("/produtos/**").uri("http://localhost:8193"))
-				.route(p -> p.path("/medidas/**").uri("http://localhost:8193"))
-				.route(p -> p.path("/categorias/**").uri("http://localhost:8193"))
+				.route(p -> p.path("/ordens/**").filters(
+						f -> f.circuitBreaker(config -> config.setName("mycmd").setFallbackUri("forward:/fallback")))
+						.uri("http://localhost:8190"))
+				.route(p -> p.path("/usuarios/**").filters(
+						f -> f.circuitBreaker(config -> config.setName("mycmd").setFallbackUri("forward:/fallback")))
+						.uri("http://localhost:8191"))
+				.route(p -> p.path("/pessoas/**").filters(
+						f -> f.circuitBreaker(config -> config.setName("mycmd").setFallbackUri("forward:/fallback")))
+						.uri("http://localhost:8192"))
+				.route(p -> p.path("/produtos/**").filters(
+						f -> f.circuitBreaker(config -> config.setName("mycmd").setFallbackUri("forward:/fallback")))
+						.uri("http://localhost:8193"))
+				.route(p -> p.path("/medidas/**").filters(
+						f -> f.circuitBreaker(config -> config.setName("mycmd").setFallbackUri("forward:/fallback")))
+						.uri("http://localhost:8193"))
+				.route(p -> p.path("/categorias/**").filters(
+						f -> f.circuitBreaker(config -> config.setName("mycmd").setFallbackUri("forward:/fallback")))
+						.uri("http://localhost:8193"))
 				.build();
 	}
 
-	@GetMapping("/fallback")
-	public String fallback() {
-		return "Por favor, tente mais tarde!";
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("Por favor, tente mais tarde!");
 	}
 
 	private static Logger log = LoggerFactory.getLogger(GatewayApplication.class);
